d9a1ef95d585c8b167433f969a7d58f15e02f227
Anmol-Srivastava-073 <anmolsrivastava073@gmail.com>
2025-08-02 16:22:01 +0530

    Update roulette.js

--- roulette.js
+++ roulette.js
@@ -1,12 +1,21 @@
 
 let num1= document.getElementById("num12");
 let but1= document.getElementById("but12");
-
+let bgSound = document.getElementById("bgSound");
 let cho= document.getElementById("fname");
 let win= document.getElementById("num11");
 let trydis= document.getElementById("ent2");
 let winSound = document.getElementById("winSound");
 let tryco=0;
+let musicStarted = false;
+cho.onclick= function(){
+    if (!musicStarted) {
+        bgSound.volume = 0.2;
+        bgSound.play(); 
+        musicStarted = true;
+    }
+}
+
 but1.onclick= function(){
     if(cho.value>0 && cho.value<31){
         let random= Math.floor(Math.random()*30 + 1);
